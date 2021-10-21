b6b78b00b0da36963672a4f1b0350ea150684e30
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2021-10-21 17:27:58 -0300

    exclusão da classe Menu

--- src/academy/devdojo/maratonajava/javacore/classes/domain/JogoForca.java
+++ src/academy/devdojo/maratonajava/javacore/classes/domain/JogoForca.java
@@ -1,12 +1,8 @@
 package academy.devdojo.maratonajava.javacore.classes.domain;
 
-import java.util.*;
-import academy.devdojo.maratonajava.javacore.classes.domain.Menu;
-
 public class JogoForca {
 
     public static void main( String[] args) {
-        Menu menu = new Menu();
-        menu.menu();
+
     }
 }
