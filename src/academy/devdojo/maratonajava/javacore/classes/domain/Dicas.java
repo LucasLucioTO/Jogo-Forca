9b3e9bd4d83b9f566e67ebe9fb0f89585194ffab
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2021-10-21 17:27:15 -0300

    criação da classe Dicas

---
+++ src/academy/devdojo/maratonajava/javacore/classes/domain/Dicas.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.classes.domain;
+
+public class Dicas {
+    String [][] facil = new String[10][];
+    String [][] medio = new String[10][];
+    String [][] dificil = new String[10][];
+
+    public Dicas(){
+        facil[0]= new String[]{"* algo recente", "* quem tem pouco tempo de existência", "* algo mvisto pela primeira vez"};
+        facil[1]= new String[]{"* residência", "* lar", "* edificação destinada a habitação"};
+        facil[2]= new String[]{"* utensilio de cozinha", "* instrumento cortante", "* usado para cortar carne"};
+        facil[3]= new String[]{"* animal que gira bola no nariz", "* seu som é parecido com uma buzina"};
+        facil[4]= new String[]{"* movel onde se come", "* movel onde se faz tarefa", "* geralmente acompanhado de cadeiras"};
+        facil[5]= new String[]{"* quantia de vendas a ser batida", "* ponto demarcado como local de chegada"};
+        facil[6]= new String[]{"* masculino de Luiza", "* primeiro nome do rei do baião"};
+        facil[7]= new String[]{"* bofetada", "* golpe no rosto de mão aberta"};
+        facil[8]= new String[]{"* veiculo de duas rodas", "* veiculo motorizado"};
+        facil[9]= new String[]{"* fica junto do pneu", "* brinquedo de criança que gira no proprio eixo"};
+
+    }
+
+}
