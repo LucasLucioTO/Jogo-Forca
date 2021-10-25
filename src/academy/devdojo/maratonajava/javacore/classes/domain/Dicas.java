6fb137914a5ca310a9d1160ee6f697920b801a52
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2021-10-25 11:19:34 -0300

    introdução das dicas médias e difíceis.

--- src/academy/devdojo/maratonajava/javacore/classes/domain/Dicas.java
+++ src/academy/devdojo/maratonajava/javacore/classes/domain/Dicas.java
@@ -16,6 +16,26 @@ public class Dicas {
         facil[7]= new String[]{"* bofetada", "* golpe no rosto de mão aberta"};
         facil[8]= new String[]{"* veiculo de duas rodas", "* veiculo motorizado"};
         facil[9]= new String[]{"* fica junto do pneu", "* brinquedo de criança que gira no proprio eixo"};
+        medio[0]= new String[]{"* feminino de Mario", "* nome da mãe de jesus"};
+        medio[1]= new String[]{"* dupla da mesma especie, sendo um masculino e outro feminino", "* marido e esposa"};
+        medio[2]= new String[]{"* jogadora de futebol feminino", "* rima com farta"};
+        medio[3]= new String[]{"* homem prestes a casar", "* parceiro da noiva"};
+        medio[4]= new String[]{"* tampa da garrafa de vinho", "* pedaço roliço de cortiça"};
+        medio[5]= new String[]{"* caixa de metal com pistões", "* que produz força para acionar maquinas"};
+        medio[6]= new String[]{"* nome de um dos apóstolos de jesus","* nome do autor desse código"};
+        medio[7]= new String[]{"* vala corrida por onde corre água.", "* Na frase:'coloca no _____ da sbt"};
+        medio[8]= new String[]{"* número que vem depois do 4.", "* quinto número."};
+        medio[9]= new String[]{"* produto de 4x5.", "* multiplo de 4 e 5."};
+        dificil[0]= new String[]{"* quarto número."};
+        dificil[1]= new String[]{"* tudo que é perceptível por qualquer dos sentidos."};
+        dificil[2]= new String[]{"* fruta amarela, branca por dentro e sem sementes."};
+        dificil[3]= new String[]{"* agasalho para se aquecer"};
+        dificil[4]= new String[]{"* simio que come banana."};
+        dificil[5]= new String[]{"* sinônimo de bela."};
+        dificil[6]= new String[]{"* que tem preço de baixo custo."};
+        dificil[7]= new String[]{"* mamifero de pescoço longo."};
+        dificil[8]= new String[]{"* tubérculo comestível rico em amido"};
+        dificil[9]= new String[]{"* pessoa recém ingressada no grupo"};
 
     }
 
