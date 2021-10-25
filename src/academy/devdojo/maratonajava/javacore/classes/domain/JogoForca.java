1e636ba57707170cfd5d8eb77ef49d8f8359c4dd
LucasLucioTO <lucasluciot.deoliveira@gmail.com>
2021-10-25 11:30:35 -0300

    finalização do jogo forca.

--- src/academy/devdojo/maratonajava/javacore/classes/domain/JogoForca.java
+++ src/academy/devdojo/maratonajava/javacore/classes/domain/JogoForca.java
@@ -1,8 +1,11 @@
 package academy.devdojo.maratonajava.javacore.classes.domain;
 
+import academy.devdojo.maratonajava.javacore.classes.domain.Forca;
 public class JogoForca {
 
-    public static void main( String[] args) {
 
+    public static void main( String[] args) {
+        Forca forca = new Forca();
+        forca.forca();
     }
 }
